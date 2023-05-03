3ae55d9861f43bf875873b81bb83a6bdfc8e428b
MatHennel <matheushennel123@gmail.com>
2023-05-03 03:11:04 -0300

    first commit

---
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,44 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.paciente.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados){
+        pacienteRepository.save(new Paciente(dados));
+        System.out.println(dados);
+    }
+
+    @GetMapping
+    public Page<DadosListagemPacientes> listar(@PageableDefault(size = 10,sort = {"nome"}) Pageable paginacao){
+        return pacienteRepository.findAllByAtivoTrue(paginacao).map(DadosListagemPacientes::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizaPaciente dados){
+        var paciente = pacienteRepository.getReferenceById(dados.id());
+        paciente.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void deletar(@PathVariable Long id){
+        var paciente = pacienteRepository.getReferenceById(id);
+        paciente.inativar();
+    }
+}
---
+++ src/main/java/med/voll/api/medico/DadosAtualizaMedico.java
@@ -0,0 +1,20 @@
+package med.voll.api.medico;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.endereco.Endereco;
+
+public record DadosAtualizaMedico(
+        @NotNull
+        Long id,
+        String nome,
+
+        @Pattern(regexp = "\\d{9,13}")
+        String telefone,
+
+        @Valid
+        DadosEndereco endereco
+) {
+}
